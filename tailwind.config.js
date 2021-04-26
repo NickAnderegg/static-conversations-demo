8220bbcb987c64302d2c19a7f19cb28f4f7fdfe8
Nick Anderegg <nick@anderegg.io>
2021-04-26 14:41:27 -0400

    checkpoint: basic blog-style site for static conversations testing
    Signed-off-by: Nick Anderegg <nick@anderegg.io>

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      flex: {
+        'l-24': '0 0 6rem'
+      },
+      fontFamily: {
+        serif: ['Source Serif Pro'],
+        mono: ['JetBrains Mono']
+      },
+      padding: {
+        '1/2': '0.125rem'
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
